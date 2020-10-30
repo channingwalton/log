4d68f4f06b9aa80aa96e425e550b1f865785824f
Channing Walton <channingwalton@mac.com>
2020-10-30 20:03:35 +0000

    making things couple

--- src/main/scala/Spike.scala
+++ src/main/scala/Spike.scala
@@ -1,26 +1,25 @@
 package io.channing
 
+import cats.implicits._
 import cats.Monad
 import cats.data.WriterT
+import LogTree._
 
 object Spike extends App {
-  import LogTree._
 
-  def foo[F[_]: Monad]: WriterT[F, Log, Int] = {
+  def foo[F[_]: Monad]: WriterT[F, Log, Int] =
     "Start Process" ~< {
       for {
         a <- doA[F]
         b <- doB[F]
       } yield a + b
     }
-  }
 
-  def doA[F[_]: Monad]: WriterT[F, Log, Int] = {
+  def doA[F[_]: Monad]: WriterT[F, Log, Int] =
     for {
       c <- 3.pure[F] ~> "Calc c"
       d <- 4.pure[F] ~> "Calc d"
     } yield c + d
-  }
 
   def doB[F[_]: Monad]: WriterT[F, Log, Int] =
     "Doing B" ~< {
@@ -30,7 +29,7 @@ object Spike extends App {
       } yield e + f
     }
 
-  val (l, v) = (foo[cats.Id].run)
+  val (l, v) = foo[cats.Id].run
   println(v)
   println(l.show)
 }
